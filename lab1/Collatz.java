6854060d173c5af5a335bfb56276d0ce2d9503eb
Exusiai114 <100024009+Exusiai101@users.noreply.github.com>
2025-11-08 22:10:29 +0800

    First Try on CS61B Lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,15 +3,31 @@
  */
 public class Collatz {
 
+    public static boolean isEven(int number) {
+        if (number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isOdd(int number) {
+        if (number % 2 == 1) {
+            return true;
+        } else  {
+            return false;
+        }
+    }
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n == 1) {
             return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        } else if (isEven(n)) {
+            return n/2;
+        }else if (isOdd(n)) {
+            return 3*n+1;
         }
+        return 1;
     }
 
     public static void main(String[] args) {
